caec0157ceed10d760172b7af8e31fe629f1d835
JulienThibeaut <julien.thibeaut@gmail.com>
2021-12-08 12:11:33 +0100

    init store

---
+++ README.md
@@ -0,0 +1,27 @@
+## Experiment with Solana & Metaplex for buildspace
+
+## Installation and Setup Instructions
+
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
+
+Add .env file with 
+
+```
+REACT_APP_CANDY_MACHINE_CONFIG=
+REACT_APP_CANDY_MACHINE_ID=
+REACT_APP_TREASURY_ADDRESS=
+REACT_APP_SOLANA_NETWORK=
+REACT_APP_SOLANA_RPC_HOST=
+```
+
+Installation:
+
+`npm install`  
+
+To Start Server:
+
+`npm start`  
+
+To Visit App:
+
+`localhost:3000`  
---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import CandyMachine from "./CandyMachine";
+
+const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          );
+
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">Christmas Trees NFT</p>
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
